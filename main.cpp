dd1cd995c28dce8f4c6667b02b305dbf2b0afea8
Cal <144805157+Carjag@users.noreply.github.com>
2024-05-08 18:06:09 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,109 +1,145 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <ctime>
-#include <random>
+// Detta ska vara ett program som likt övningsprovet innefattar stora mängder av det som kommer på framtida programmeringsprov. 
+// Viktiga delar är variabler, operatorer, selektion, loopar, funktioner, arrayer och structer.
+// Andra viktiga delar innefattar referensparameter, typer av funktioner bl.a.
 
+#include <iostream>
 using namespace std;
 
-int getRandomNumber() {
+struct Glass {
 
-	 // Använder random device för att seeda Mersenne Twister engine
-	random_device rd;
-	mt19937 mt(rd());
+	string Gsmak;
+	string Gstrut;
+	string Gkulor;
 
-	// Genererar ett random nummer mellan 1 och 10
-	uniform_int_distribution<int> distribution(1, 10);
-	int randomNumber = distribution(mt);
+}; // Här hade man redan nu kunnat skapa structer, men eftersom att vi vill att individen som kör programmet själv ska få göra detta, så väntar vi med att initialisera structer. 
 
-	return randomNumber;
-}
-
-void printMessage(string message) { // Skickar meddelandet till konsolen
-
-	cout << message;
-
-}
-
-int calculateSum(int num1, int num2) { // adderar variablerna num1 & num2
-
-	return num1 + num2;
-
-}
-
-void swapNumbers(int &num1, int &num2) { // byter referensvariablernas värde
-
-	swap(num1, num2);
-
-}
-
-double calculateAverage(int arr[], int size) { // kalkylerar medelvärdet genom att addera värdena i arrayen och dela dessa med storleken. 
-
-	int i, sum = 0;
-
-	for (i = 0; i < size; ++i) {
-		sum += arr[i];
-	}
-	return double(sum) / size;
-
-}
-
-void reversearray(int arr[], int size) {
-
-	for (int i = 0; i < size / 2; i++) // Byter ordningen på arrayen med en loop och funktionen swap. 
-		swap(arr[i], arr[size - i - 1]);
-}
+void sokSmak(Glass g[], int antal);
+void sokStrut(Glass g[], int antal);
+void sokKulor(Glass g[], int antal); 
+void meny(Glass g[], int antal);
+void resultat(Glass g[], int i);
 
 int main() {
 
-	string name = "Calle";
-	string message = name + " is learning C++ functions, loops, and more!\n";
-	printMessage(message); // skickar meddelandet till printMessage som skriver ut det
+	system("chcp 1252"); // Använder vokalerna å, ä och ö.
 
-	int num1 = getRandomNumber(); // genererar 1 random siffra
-	cout << num1 << "\n";
-	int num2 = getRandomNumber(); // genererar ytterligare en random siffra
-	cout << num2 << "\n";	
+	Glass minaGlassar[5]; // Skapar en array som en struct, som max kan innehålla 5 olika element. (3 * 5 = 15 olika val på smak, strut och kulor tillsammans).
+	int antal = 0;
+	cout << "Hur många olika glassar vill du ha i din struct?" << endl;
+	cout << "(5 glassar får plats) - : ";
+	cin >> antal;
 
-	int total = calculateSum(num1, num2);
-	cout << total << "\n"; // Skriver ut summan av de två talen num1 och num2
+	for (int i = 0; i < antal; i++) { // loop som körs (antal) gånger där man får lägga in olika val beroende på hur många glassar man vill skapa. 
+		cout << "Vilken smak ska glass nr " << i + 1 << " ha? : ";
+		cin >> minaGlassar[i].Gsmak; // Skriver in en struct med namnet minaGlassar[i], där i går från 0 < antal (lägger på 1 för att den inte ska starta från 0 i programmet).
+		cout << "Vad vill du ha glass nr " << i + 1 << " i? : ";
+		cin >> minaGlassar[i].Gstrut;
+		cout << "Hur många kulor ska glass nr " << i + 1 << " ha? : ";
+		cin >> minaGlassar[i].Gkulor;
+	}
+	meny(minaGlassar, antal); // Skickar Arrayerna och antalet arrayer till funktionen meny. 
 
-	for (int i = 0; i < 10; i++) { // Loop med random nummer och antal iterationer av denna.
-		
-		int num3 = getRandomNumber();
-		cout << num3 << " Iteration: " << (i + 1) << "\n";
+	return 0;
+}
+
+void meny(Glass g[], int antal) { // Skriver endast Glass g[], behöver inte skriva [struct] före Glass. 
+
+	char val; //Använder char för att variabeln sparar en enskild bokstav, vilket vi i detta fall använder oss av. (Med ASCII).
+	cout << "\nVälkommen till menyn! Här kan du söka på glassarna du har skapat !" << endl;
+	cout << "Sök Glass!" << endl;
+	cout << "Vill du söka på Smak, välj [s]" << endl;
+	cout << "Vill du söka på variant av strut, välj [t]" << endl;
+	cout << "vill du söka på antal kulor, välj [k]" << endl;
+	cout << " --> : ";
+
+	cin >> val;
+
+	switch (val) { // En typ av selektion där ett uttryck utvärderas och jämförs med olika scenarion. Om ett scenario stämmer, så körs koden under.
+	case 's':
+		sokSmak(g, antal);
+		break; // används för att ignorera resten av koden i switch-blocket om det första utfallet var det sökta. 
+	case 't':
+		sokStrut(g, antal);
+		break;
+	case 'k':
+		sokKulor(g, antal);
+		break;
+	default:
+		cout << "Ogiltigt val!" << endl;
+
+		meny(g, antal);
 
 	}
 
-	int myNumbers[5];
-	cout << "Your initial array is: "; // Randomiserar 5 nummer som läggs in i arrayen då i ökar med 1 varje gång ett nummer lagts till.
-	for (int i = 0; i < 5; i++) {
+}
 
-		myNumbers[i] = getRandomNumber();
-		cout << myNumbers[i] << " ";
-	}
+void sokSmak(Glass g[], int antal) { // Funktion som hanterar sökandet av smak. Använder arrayen minaGlassar som skickas till meny från main funktionen.
 
-	double Average = calculateAverage(myNumbers, 5); // skickar arrayen och antal nummer inkluderat till calculateAverage som räknar ut medelvärdet av siffrorna.
-	cout << "\nYour average is: " << Average;
+	string sok;
+	cout << "Sök på Smak: ";
+	cin >> sok;
 
-	cout << "\nThe reversed array is: "; // Skickar arrayen och storleken till funktionen reversearray som byter ordning på alla siffror. 
-	reversearray(myNumbers, 5);
-	for (int i = 0; i < 5; i++) {
-		cout << myNumbers[i] << " ";
-	}
-
-	cout << "\nThe swapped array (compared to the reversed) is: "; // Skickar två nummer från arrayen till swapNumbers funktionen som byter dessa. 
-	swapNumbers(myNumbers[2], myNumbers[4]);
-	for (int i = 0; i < 5; i++) {
-		cout << myNumbers[i] << " "; // Skriver den nya arrayen till konsolen.
-	}
-	cout << "\nComplex loop with function calls: "; // Genererar random nummer som adderas till summan överstiger eller är 100. 
 	int i = 0;
-	while (i < 100) {
-		i += getRandomNumber();
-		cout << i << " ";
+	while (i < antal) {
+		if (sok == g[i].Gsmak) { // Söker på om string sok är samma som Gsmak för en av de tidigare valda glassarna.
+			resultat(g, i);
+			meny(g, antal);
+		}
+
+		i++;
 	}
-	string limit = "Limit reached"; // Skickar meddelandet till funktionen printMessage som skriver det till konsolen.
-	printMessage(limit);
+	cout << "Den sökta glassen hittas inte." << endl;
+
+	meny(g, antal);
+}
+
+void sokStrut(Glass g[], int antal) { // Samma funktion för variant av behållare / bägare ...
+
+	string sok;
+	cout << "Sök på variant av behållare: ";
+	cin >> sok;
+
+	int i = 0;
+	while (i < antal) {
+		if (sok == g[i].Gstrut) {
+			resultat(g, i);
+			meny(g, antal);
+		}
+
+		i++;
+	}
+	cout << "Den sökta glassen hittas inte." << endl;
+
+	meny(g, antal);
+}
+
+void sokKulor(Glass g[], int antal) { // Samma funktion fast för antal kulor. 
+
+	string sok;
+	cout << "Sök på antal kulor: ";
+	cin >> sok;
+
+	int i = 0;
+	while (i < antal) {
+		if (sok == g[i].Gkulor) {
+			resultat(g, i);
+			meny(g, antal);
+		}
+
+		i++;
+	}
+	cout << "Den sökta glassen hittas inte." << endl;
+
+	meny(g, antal);
+}
+
+void resultat(Glass g[], int i) { // Funktion som skriver ut den sökta glassen, beroende på vilken som tidigare hittades i if satserna för de tre olika valen. 
+	// Därefter skickas arrayen g (som från början är minaGlassar) samt variabeln i som har värdet av det som if-satsen fann sant. 
+	// På så vis skrivs bara de korrekta värdena nu i konsolen.
+
+	cout << "\nDen sökta glassen är: " << endl;
+	cout << "Smak: " << g[i].Gsmak << endl;
+	cout << "Behållare: " << g[i].Gstrut << endl;
+	cout << "Antal kulor: " << g[i].Gkulor << endl;
 
 }
